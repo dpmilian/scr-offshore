c3ad33134bb8e089811b292c333ad9a913235b9e
Daniel Perez <dperez@affinimeter.com>
2018-12-03 14:07:02 +0100

    Added RPLidar class for integration into the GUI, does 10 spins and spits out values before starting main HUD, just a test! See if it ports to VM

---
+++ fancy-hud/scrlidar2d.cpp
@@ -0,0 +1,6 @@
+#include "scrlidar2d.h"
+
+scrlidar2d::scrlidar2d()
+{
+
+}
---
+++ fancy-hud/scrlidar2d.h
@@ -0,0 +1,11 @@
+#ifndef SCRLIDAR2D_H
+#define SCRLIDAR2D_H
+
+
+class scrlidar2d
+{
+public:
+    scrlidar2d();
+};
+
+#endif // SCRLIDAR2D_H
